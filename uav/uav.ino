dc04209d0da0f0c13381ac7787041798ea8c2028
Oleg Titov <olvtitov@gmail.com>
2013-10-08 00:37:28 +0300

    2

--- uav/uav.ino
+++ uav/uav.ino
@@ -1,21 +1,27 @@
 // Version 0.1
 //0 = no key pressed;
-//1 = UP key pressed;
-//2 = DOWN key pressed;
-//3 = LEFT key pressed;
-//4 = RIGHT key pressed;
+//1, '1' = UP key pressed;
+//2, '2' = DOWN key pressed;
+//3, '3' = LEFT key pressed;
+//4, '4' = RIGHT key pressed;
 
 
 int pinForward = 10;
 int pinRevers  = 9;
-int pinRight   = 11;
-int pinLeft    = 12;
+int pinFR      = 11;
+
+int pinRight   = 2;
+int pinLeft    = 3;
+int pinLR      = 4;
 
 
-int maxN=1;
+int maxN=5;
 int cmd=0;
 
-int FR=0; // -3,-2,-1, 0, 1, 2, 3
+int maxC=1023;
+int minC=748;
+
+int FR=0; // -maxN, -3,-2,-1, 0, 1, 2, 3, maxN
 int LR=0;
   
   
@@ -28,12 +34,20 @@ void setup()
   
   pinMode(pinRevers, OUTPUT);  
   digitalWrite(pinRevers, LOW);
+  
+  pinMode(pinFR, OUTPUT);  
+  analogWrite(pinFR, LOW);
+
+
 
   pinMode(pinRight, OUTPUT);  
   digitalWrite(pinRight, LOW);
 
   pinMode(pinLeft, OUTPUT);  
   digitalWrite(pinLeft, LOW);
+
+  pinMode(pinFR, OUTPUT);  
+  analogWrite(pinLR, LOW);
 
 
   Serial.println("RESET");  
@@ -52,30 +66,28 @@ void loop()
   if (Serial.available() > 0) {  
     cmd = Serial.read(); 
     
-    if (cmd>47){
-                 cmd = cmd-48;
-               }
+    if (cmd>47){ cmd = cmd-48; }  //1 == '1'
     
     switch (cmd) { 
       case 1:
               if (FR<maxN){ FR=FR+1; }
-              SetPinAct(FR,pinForward,pinRevers);
+              SetPinAct(FR, pinForward, pinRevers, pinFR);
               Serial.println("1:Forward");  
               break;  
       case 2:
               if (FR>-maxN){ FR=FR-1; }
-              SetPinAct(FR,pinForward,pinRevers);
+              SetPinAct(FR, pinForward, pinRevers, pinFR);
               Serial.println("2:Revers"); 
               break;  
 
 
       case 3:
               if (LR<maxN){ LR=LR+1; }
-              SetPinAct(LR,pinLeft,pinRight);
+              SetPinAct(LR, pinLeft, pinRight, pinLR);
               break;  
       case 4:
               if (LR>-maxN){ LR=LR-1; }
-              SetPinAct(LR,pinLeft,pinRight);
+              SetPinAct(LR, pinLeft, pinRight, pinLR);
               break;  
 
       default: 
@@ -87,18 +99,26 @@ void loop()
 
 }
 
-void SetPinAct(int Flag, int pin1, int pin2){
+void SetPinAct(int Flag, int pin1, int pin2, int pin3){
+  
   if (Flag>0) {
      digitalWrite(pin1, HIGH); 
      digitalWrite(pin2, LOW);
+     analogWrite(pin3, minC+Flag*(maxC-minC)/maxN);
+     
+     
   }
   if (Flag==0) {
      digitalWrite(pin1, LOW); 
      digitalWrite(pin2, LOW);
+     analogWrite(pin3, 0);
+     
   }
   if (Flag<0) {
      digitalWrite(pin1, LOW); 
      digitalWrite(pin2, HIGH);
+     analogWrite(pin3, minC-Flag*(maxC-minC)/maxN);
+
   }
 
 }
